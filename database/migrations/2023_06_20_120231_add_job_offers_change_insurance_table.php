e72248b7619cdd5a6dabe787e263ac261a7dc44f
taiseisugai <>
2023-06-20 17:49:50 +0900

    null可に変更

---
+++ database/migrations/2023_06_20_120231_add_job_offers_change_insurance_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeNegotiationRankjobOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('job_offers', function (Blueprint $table) {
+            $table->string('employment_insurance_2', 100)->comment('雇用保険加入②');->nullable(true)->change();//
+            $table->string('social_insurance_2', 100)->comment('社会保険加入②');->nullable(true)->change();//
+            $table->string('employment_insurance_3', 100)->comment('雇用保険加入③');->nullable(true)->change();//
+            $table->string('social_insurance_3', 100)->comment('社会保険加入③');->nullable(true)->change();//
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('job_offers', function (Blueprint $table) {
+            $table->string('employment_insurance_2', 100)->comment('雇用保険加入②');->nullable(false)->change();;//
+            $table->string('social_insurance_2', 100)->comment('社会保険加入②');->nullable(false)->change();;//
+            $table->string('employment_insurance_3', 100)->comment('雇用保険加入③');->nullable(false)->change();;//
+            $table->string('social_insurance_3', 100)->comment('社会保険加入③');->nullable(false)->change();;//
+        });
+    }
+}
