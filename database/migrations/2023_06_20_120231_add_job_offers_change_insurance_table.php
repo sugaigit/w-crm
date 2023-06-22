5365ad8d398eb738e86337b2d4c697a03056d66c
hiraokakoichi <the10pointsflavor0327@gmail.com>
2023-06-22 17:18:03 +0900

    マイグレーションの文法エラー解消

--- database/migrations/2023_06_20_120231_add_job_offers_change_insurance_table.php
+++ database/migrations/2023_06_20_120231_add_job_offers_change_insurance_table.php
@@ -14,10 +14,10 @@ class ChangeNegotiationRankjobOffersTable extends Migration
     public function up()
     {
         Schema::table('job_offers', function (Blueprint $table) {
-            $table->string('employment_insurance_2', 100)->comment('雇用保険加入②');->nullable(true)->change();//
-            $table->string('social_insurance_2', 100)->comment('社会保険加入②');->nullable(true)->change();//
-            $table->string('employment_insurance_3', 100)->comment('雇用保険加入③');->nullable(true)->change();//
-            $table->string('social_insurance_3', 100)->comment('社会保険加入③');->nullable(true)->change();//
+            $table->string('employment_insurance_2', 100)->comment('雇用保険加入②')->nullable(true)->change();//
+            $table->string('social_insurance_2', 100)->comment('社会保険加入②')->nullable(true)->change();//
+            $table->string('employment_insurance_3', 100)->comment('雇用保険加入③')->nullable(true)->change();//
+            $table->string('social_insurance_3', 100)->comment('社会保険加入③')->nullable(true)->change();//
         });
     }
 
@@ -29,10 +29,10 @@ class ChangeNegotiationRankjobOffersTable extends Migration
     public function down()
     {
         Schema::table('job_offers', function (Blueprint $table) {
-            $table->string('employment_insurance_2', 100)->comment('雇用保険加入②');->nullable(false)->change();;//
-            $table->string('social_insurance_2', 100)->comment('社会保険加入②');->nullable(false)->change();;//
-            $table->string('employment_insurance_3', 100)->comment('雇用保険加入③');->nullable(false)->change();;//
-            $table->string('social_insurance_3', 100)->comment('社会保険加入③');->nullable(false)->change();;//
+            $table->string('employment_insurance_2', 100)->comment('雇用保険加入②')->nullable(false)->change();;//
+            $table->string('social_insurance_2', 100)->comment('社会保険加入②')->nullable(false)->change();;//
+            $table->string('employment_insurance_3', 100)->comment('雇用保険加入③')->nullable(false)->change();;//
+            $table->string('social_insurance_3', 100)->comment('社会保険加入③')->nullable(false)->change();;//
         });
     }
 }
