9d1b3fc23467b5d673a90fdcc4c28768c8fa39c3
yamamoto <yamamoto.0603@plusad.co.jp>
2023-08-24 18:03:32 +0900

    表のスクロール化修正

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -52,43 +52,44 @@
         </select>
     </div>
 </div>
-
-<table class="table table-bordered table-hover w-75 m-auto">
-    <thead>
-        <tr class=m-auto style="background-color: lightgray">
-            <th class="text-center">顧客ID</th>
-            <th class="text-center">取扱会社種別</th>
-            <th class="text-center">取扱事業所</th>
-            <th class="text-center">企業ランク</th>
-            <th class="text-center">法人形態</th>
-            <th class="text-center">顧客名</th>
-            <th class="text-center">業種</th>
-            <th class="text-center">住所</th>
-            <th class="text-center">作成者</th>
-            <th class="text-center">操作</th>
+<div class="table_wrap" style="overflow: auto; max-height: 400px; max-width: 75%; margin: 0 auto;">
+    <table class="table table-bordered table-hover m-auto" style="overflow-x: auto; white-space: nowrap; margin-bottom: 0px;">
+        <thead>
+            <tr class=m-auto style="background-color: lightgray">
+                <th class="text-center">顧客ID</th>
+                <th class="text-center">取扱会社種別</th>
+                <th class="text-center">取扱事業所</th>
+                <th class="text-center">企業ランク</th>
+                <th class="text-center">法人形態</th>
+                <th class="text-center">顧客名</th>
+                <th class="text-center">業種</th>
+                <th class="text-center">住所</th>
+                <th class="text-center">作成者</th>
+                <th class="text-center">操作</th>
+            </tr>
+        </thead>
+        @foreach($customers as $customer)
+        <tr>
+            <td>{{ $customer->id }}</td>
+            <td>{{ !empty($customer->handling_type) ? config('options')['handling_type'][$customer->handling_type] :'' }}</td>
+            <td>{{ !empty($customer->handling_office) ? config('options')['handling_office'][$customer->handling_office] :'' }}</td>
+            <td>{{ $customer->rank }}</td>
+            <td>{{ !empty($customer->corporate_type) ? config('options')['corporate_type'][$customer->corporate_type] :'' }}</td>
+            <td><a href="{{ route('job_offers.index', ['customerName' => $customer->customer_name]) }}">{{ $customer->customer_name }}</a></td>
+            <td>{{ !empty($customer->industry) ? config('options')['industry'][$customer->industry] :'' }}</td>
+            <td>{{ $customer->address }}</td>
+            <td>{{ $customer->user->name }}</td>
+            <td>
+                <div class="d-flex justify-content-around">
+                    <a href="{{ route('customers.detail', $customer->id) }}">
+                        <button class="btn btn-primary" type="button">詳細</button>
+                    </a>
+                </div>
+            </td>
         </tr>
-    </thead>
-    @foreach($customers as $customer)
-    <tr>
-        <td>{{ $customer->id }}</td>
-        <td>{{ !empty($customer->handling_type) ? config('options')['handling_type'][$customer->handling_type] :'' }}</td>
-        <td>{{ !empty($customer->handling_office) ? config('options')['handling_office'][$customer->handling_office] :'' }}</td>
-        <td>{{ $customer->rank }}</td>
-        <td>{{ !empty($customer->corporate_type) ? config('options')['corporate_type'][$customer->corporate_type] :'' }}</td>
-        <td><a href="{{ route('job_offers.index', ['customerName' => $customer->customer_name]) }}">{{ $customer->customer_name }}</a></td>
-        <td>{{ !empty($customer->industry) ? config('options')['industry'][$customer->industry] :'' }}</td>
-        <td>{{ $customer->address }}</td>
-        <td>{{ $customer->user->name }}</td>
-        <td>
-            <div class="d-flex justify-content-around">
-                <a href="{{ route('customers.detail', $customer->id) }}">
-                    <button class="btn btn-primary" type="button">詳細</button>
-                </a>
-            </div>
-        </td>
-    </tr>
-    @endforeach
-</table>
+        @endforeach
+    </table>
+</div>
 
 <div class="d-flex justify-content-center mt-2">
     {{ $customers->links() }}
