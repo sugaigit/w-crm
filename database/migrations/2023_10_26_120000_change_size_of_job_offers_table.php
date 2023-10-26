c214ff09dff324dd1f57562e489d60b87d3ddcba
hiraokakoichi <the10pointsflavor0327@gmail.com>
2023-10-26 17:47:52 +0900

    マイグレーションファイル作成

---
+++ database/migrations/2023_10_26_120000_change_size_of_job_offers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeSizeOfJobOffersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('job_offers', function (Blueprint $table) {
+            $table->string('company_address', 200)->change();
+            $table->string('company_others', 200)->change();
+            $table->text('bonuses_treatment')->change();
+            $table->text('holidays_vacations')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('job_offers', function (Blueprint $table) {
+            $table->string('company_address', 100)->change();
+            $table->string('company_others', 100)->change();
+            $table->string('bonuses_treatment', 255)->change();
+            $table->string('holidays_vacations', 255)->change();
+        });
+    }
+}
